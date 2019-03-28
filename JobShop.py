77055715a89af9468d25686b5f1f52b9a89add8e
ZelinZang_Mac <zangzelin@gmail.com>
2019-03-28 22:09:34 +0800

    finish my own subnetwork, but only one feature

---
+++ JobShop.py
@@ -0,0 +1,282 @@
+import numpy as np
+import random
+import time
+import matplotlib.pyplot as plt
+
+
+class JobShop:
+
+    number_job = None
+    number_machine = None
+    number_features = None
+
+    time_low = None
+    time_high = None
+
+    M_processing_time = None
+    M_processing_order = None
+    M_start_time = None
+    M_end_time = None
+    X_schedule_plan = None
+    schedule_line = None
+
+    def __init__(self, number_machine, number_job, time_low, time_high):
+        self.number_job = number_job
+        self.number_machine = number_machine
+        self.time_low = time_low
+        self.time_high = time_high
+        self.schedule_line = []
+        self.GenerateRandomProblem()
+
+    def Get_Possible_Job_Position(self):
+        job_position_list = [ 0 for i in range(self.number_job)]
+        for job_id, job_position in self.schedule_line:
+            if job_position < self.number_machine-1:
+                job_position_list[job_id] = job_position+1
+            else:
+                job_position_list[job_id] = -1
+
+        return [ [i,job_position_list[i]] for i in range(len(job_position_list))]
+    
+    def Get_Features(self, possible_job_position):
+        featrues  = []
+        for job_id, job_position in possible_job_position:
+            f_item = self.GetFeature(job_id,job_position)
+            featrues.append( f_item )
+
+        return featrues
+    def Step(self, action = None):
+        done = False
+        if action == None:
+            self.MeasurementAction(self.schedule_line)
+            possible_pob_position = self.Get_Possible_Job_Position()
+            state =  np.array(self.Get_Features(possible_pob_position))
+            score = 0
+        else:
+            
+            job_position_list = [ 0 for i in range(self.number_job)]
+            for job_id, job_position in self.schedule_line:
+                if job_position < self.number_machine-1:
+                    job_position_list[job_id] = job_position+1
+                else:
+                    job_position_list[job_id] = -1
+            if job_position_list[action] == -1:
+                done = True
+                canchoose = [[i,job_position_list[i]] for i in range(self.number_job) if job_position_list[i] != -1 ]
+                action = canchoose[0]
+            else:
+                action = [action,job_position_list[action]]
+           
+            self.schedule_line.append(action)
+            self.MeasurementAction(self.schedule_line)
+            score = np.max(self.M_end_time)
+            
+            possible_pob_position = self.Get_Possible_Job_Position()
+            state =  np.array(self.Get_Features(possible_pob_position))
+
+        return state, score,done
+
+    def GenerateRandomProblem(self):
+        a = list(range(self.time_low, self.time_high))
+        p = []
+        for k in range(self.number_job):
+            p.append(random.sample(a, self.number_machine))
+        self.M_processing_time = np.array(p)
+
+        a = list(range(self.number_machine))
+        r = []
+        for k in range(self.number_job):
+            r.append(random.sample(a, self.number_machine))
+        self.M_processing_order = np.array(r)
+
+        sum_time_of_job = np.sum(self.M_processing_time, axis=1)
+
+        for i in range(self.number_job):
+            for j in range(i+1, self.number_job):
+                if sum_time_of_job[i] > sum_time_of_job[j]:
+                    a = np.copy(self.M_processing_time[j, :])
+                    self.M_processing_time[j, :] = self.M_processing_time[i, :]
+                    self.M_processing_time[i, :] = a
+                    sum_time_of_job[i], sum_time_of_job[j] = sum_time_of_job[j], sum_time_of_job[i]
+
+        sum_time_of_mach = [[i, 0] for i in range(self.number_machine)]
+        for i in range(self.number_job):
+            for j in range(self.number_machine):
+                sum_time_of_mach[self.M_processing_order[i, j]
+                                 ][1] += self.M_processing_time[i, j]
+
+        for i in range(self.number_machine):
+            for j in range(i+1, self.number_machine):
+                if sum_time_of_mach[i][1] > sum_time_of_mach[j][1]:
+                    sum_time_of_mach[i], sum_time_of_mach[j] = sum_time_of_mach[j], sum_time_of_mach[i]
+
+        nr = np.zeros((self.number_job, self.number_machine), dtype=int)-1
+        for i in range(self.number_machine):
+            nr[self.M_processing_order == i] = sum_time_of_mach[i][0]
+
+        sum_time_of_mach = [[i, 0] for i in range(self.number_machine)]
+        for i in range(self.number_job):
+            for j in range(self.number_machine):
+                sum_time_of_mach[self.M_processing_order[i, j]
+                                 ][1] += self.M_processing_time[i, j]
+
+        self.M_processing_order = nr
+        self.M_processing_order = np.array([[1, 3, 0, 2], [0, 2, 1, 3], [3, 1, 2, 0], [1, 3, 0, 2], [0, 1, 2, 3]])
+        self.M_processing_time = np.array([[18, 20, 21, 17], [18, 26, 15, 16], [17, 18, 27, 23], [18, 21, 25, 15], [22, 29, 28, 21]])
+    def MeasurementAction(self, action_history):
+
+        M_start_time = np.zeros((self.number_machine, self.number_job))
+        M_end_time = np.zeros((self.number_machine, self.number_job))
+
+        timeline_machine = np.zeros((self.number_machine), dtype=int)
+        index_machine = np.zeros((self.number_machine), dtype=int)
+        timeline_job = np.zeros((self.number_job), dtype=int)
+        index_job = np.zeros((self.number_job), dtype=int)
+        X_schedule_plan = np.zeros(
+            (self.number_machine, self.number_job, 2), dtype=int)
+
+        for job_id, job_position in action_history:
+            machine_id = self.M_processing_order[job_id, job_position]
+
+            current_start_time = max(
+                timeline_machine[machine_id], timeline_job[job_id])
+            current_end_time = current_start_time + \
+                self.M_processing_time[job_id, job_position]
+
+            timeline_machine[machine_id], timeline_job[job_id] = current_end_time, current_end_time
+            current_index = index_machine[machine_id]
+            M_start_time[machine_id, current_index] = current_start_time
+            M_end_time[machine_id, current_index] = current_end_time
+            X_schedule_plan[machine_id, current_index, :] = [
+                job_id, job_position]
+            index_machine[machine_id] += 1
+            index_job[job_id] += 1
+
+        self.M_start_time = M_start_time
+        self.M_end_time = M_end_time
+        self.X_schedule_plan = X_schedule_plan
+        return np.max(M_end_time)
+
+    def PlotResult(self, num=0):
+
+        colorbox = ['yellow', 'whitesmoke', 'lightyellow',
+                    'khaki', 'silver', 'pink', 'lightgreen', 'orange', 'grey', 'r', 'brown']
+
+        for i in range(100):
+            colorArr = ['1', '2', '3', '4', '5', '6', '7',
+                        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+            color = ""
+            for i in range(6):
+                color += colorArr[random.randint(0, 14)]
+            colorbox.append("#"+color)
+
+        fig = plt.figure(figsize=(7, 4))
+        for i in range(self.number_machine):
+            # number_of_mashine:
+            for j in range(self.number_job):
+                # number_of_job:
+                # % read the start time point
+                mPoint1 = self.M_start_time[i, j]
+                mPoint2 = self.M_end_time[i, j]  # % read the end time point
+                mText = i + 1.5  # % read the index of machine
+                PlotRec(mPoint1, mPoint2, mText)  # % plot subfunction
+                Word = str(self.X_schedule_plan[i, j, 0]+1) + '.' + str(
+                    self.X_schedule_plan[i, j, 1]+1)  # % read machine id
+
+                x1, x2, x3, x4 = mPoint1, mPoint2, mPoint2, mPoint1
+                y1, y2, y3, y4 = mText-0.8, mText-0.8, mText, mText
+                plt.fill([x1, x2, x3, x4], [y1, y2, y3, y4],
+                         color=colorbox[self.X_schedule_plan[i, j, 0]])
+
+                plt.text(0.5*mPoint1+0.5*mPoint2-3.5, mText-0.5, Word)
+
+        plt.xlabel('Time')
+        plt.ylabel('Machine')
+        plt.tight_layout()
+        plt.savefig('gant.png')
+
+    def Print_info(self):
+        print('order')
+        print(self.M_processing_order)
+        print('time')
+        print(self.M_processing_time)
+        print('start time')
+        print(self.M_start_time)
+        print('end time')
+        print(self.M_end_time)
+        print('X')
+        print(self.X_schedule_plan)
+
+    def GetJobCompletionPersentage(self, job_id,job_position):
+        job_time_need = np.sum(self.M_processing_time,axis=1)
+        job_position_time_need = self.M_processing_time[job_id,job_position]
+        return job_position_time_need/job_time_need[job_id] 
+
+    def GetFeature(self,job_id,job_position):
+        # raw features
+        machine_id = self.M_processing_order[job_id, job_position]
+        job_time_need = np.sum(self.M_processing_time,axis=1)
+        current_time_use = self.M_processing_time[job_id,job_position]
+
+        Machine_endtime = np.max(self.M_end_time,axis=1)
+
+        if job_position ==0:
+            frac_currentend_othermachineave = 1
+            frac_currentendplusthisposition_othermachineave = 1
+            schedule_finish_station = 0
+
+            frac_jobposition_jobtime = 1
+            frac_jobposition_totaltime = 1
+        else:
+            frac_currentend_othermachineave = (0.1+Machine_endtime[machine_id]) / (0.1+np.average(Machine_endtime))
+            frac_currentendplusthisposition_othermachineave = (Machine_endtime[machine_id]+current_time_use)/np.average(Machine_endtime)
+            schedule_finish_station = np.count_nonzero(self.M_end_time)/self.number_machine/self.number_job
+
+            frac_jobposition_jobtime = current_time_use/job_time_need[job_id]
+            frac_jobposition_totaltime = current_time_use/np.sum(job_time_need)
+
+
+        # feature choose
+        features = []
+        # current features
+        features.append(frac_currentend_othermachineave)
+        # features.append(frac_currentendplusthisposition_othermachineave)
+        # features.append(schedule_finish_station)
+        # # stable features
+        # features.append(frac_jobposition_jobtime) 
+        # features.append(frac_jobposition_totaltime)
+
+        self.number_features = len(features)
+        
+        if job_position ==-1:
+            features = [-1]* self.number_features
+        
+        return features
+
+
+def PlotRec(mPoint1, mPoint2, mText):
+
+    vPoint = np.zeros((4, 2))
+    vPoint[0, :] = [mPoint1, mText-0.8]
+    vPoint[1, :] = [mPoint2, mText-0.8]
+    vPoint[2, :] = [mPoint1, mText]
+    vPoint[3, :] = [mPoint2, mText]
+    plt.plot([vPoint[0, 0], vPoint[1, 0]], [vPoint[0, 1], vPoint[1, 1]], 'k')
+    plt.plot([vPoint[0, 0], vPoint[2, 0]], [vPoint[0, 1], vPoint[2, 1]], 'k')
+    plt.plot([vPoint[1, 0], vPoint[3, 0]], [vPoint[1, 1], vPoint[3, 1]], 'k')
+    plt.plot([vPoint[2, 0], vPoint[3, 0]], [vPoint[2, 1], vPoint[3, 1]], 'k')
+
+
+if __name__ == "__main__":
+    localtime = time.asctime(time.localtime(time.time()))
+    print(localtime)
+
+    problem = JobShop(4, 5, 15, 30)
+    # print(problem.MeasurementAction([]))
+    print(problem.MeasurementAction([[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], 
+                                     [0, 1], [1, 1], [2, 1], [3, 1], [4, 1], ]))
+                                    #  [0, 2], [1, 2], [2, 2], [3, 2], [4, 2], 
+                                    #  [0, 3], [1, 3], [2, 3], [3, 3], [4, 3], 
+    problem.GetFeature(0,0)
+    # problem.Print_info()
+    # problem.PlotResult()
---
+++ dqnwork2.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+# with my sub network by only have one feature.
+import random
+from collections import deque
+
+import gym
+import keras
+import numpy as np
+from keras.layers import Dense, Input
+from keras.models import Model, Sequential
+from keras.optimizers import Adam
+from keras.utils import plot_model
+
+import JobShop
+
+EPISODES = 10000
+
+
+class DQNAgent:
+    def __init__(self, state_size, action_size, number_job, number_feature):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.number_job = number_job
+        self.number_feature = number_feature
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 0.9  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+
+    def _build_model(self):
+        basic_model = self._basic_model()
+        # basic_model = Dense(10)
+
+        output_list = []
+        input_list = []
+        for i in range(self.number_job):
+            input_list.append(Input(shape=(self.number_feature,)))
+            output_list.append(basic_model(input_list[i]))
+
+        concatenated = keras.layers.concatenate(output_list)
+        out = Dense(self.action_size, activation='linear')(concatenated)
+        model = Model(input_list, out)
+        model.compile(loss='mse',
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def _basic_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential(name='basic_model')
+        model.add(Dense(24, input_dim=self.number_feature, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(1, activation='linear'))
+        model.compile(loss='mse',
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
+    # def _build_model(self):
+    #     # Neural Net for Deep-Q learning Model
+    #     model = Sequential() 
+    #     model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+    #     model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+    #     model.add(Dense(self.action_size, activation='linear'))
+    #     model.compile(loss='mse',
+    #                   optimizer=Adam(lr=self.learning_rate))
+    #     return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        # input(act_values)
+        return np.argmax(act_values[0])  # returns action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax( self.model.predict(  next_state )[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
+
+# if __name__ == "__main__":
+#     env = gym.make('CartPole-v1')
+#     state_size = env.observation_space.shape[0]
+#     action_size = env.action_space.n
+#     agent = DQNAgent(state_size, action_size)
+#     # agent.load("./save/cartpole-dqn.h5")
+#     done = False
+#     batch_size = 32
+
+#     for e in range(EPISODES):
+#         state = env.reset()
+#         state = np.reshape(state, [1, state_size])
+#         for time in range(500):
+#             # env.render()
+#             action = agent.act(state)
+#             next_state, reward, done, _ = env.step(action)
+#             reward = reward if not done else -10
+#             next_state = np.reshape(next_state, [1, state_size])
+#             agent.remember(state, action, reward, next_state, done)
+#             state = next_state
+#             if done:
+#                 print("episode: {}/{}, score: {}, e: {:.2}"
+#                       .format(e, EPISODES, time, agent.epsilon))
+#                 break
+#             if len(agent.memory) > batch_size:
+#                 agent.replay(batch_size)
+#         # if e % 10 == 0:
+#         #     agent.save("./save/cartpole-dqn.h5")
+
+if __name__ == "__main__":
+    number_job = 5
+    number_machine = 4
+    number_feature = 1
+
+    # env = gym.make('CartPole-v1')
+    state_size = number_job * number_feature
+    action_size = number_job
+    agent = DQNAgent(state_size, action_size, number_job, number_feature)
+    # agent.load("./save/cartpole-dqn.h5")
+    done = False
+    batch_size = number_job * number_machine * 10
+
+    history = []
+
+    for e in range(EPISODES):
+        problem = JobShop.JobShop(number_machine, number_job, 15, 30)
+        state, score, done = problem.Step()
+        state = np.reshape(state, [1,state_size,1])
+        state = [state[0][i] for i in range(number_job)]
+        score = 0
+        oldscore = 0
+
+        action_list = []
+        for time in range(number_job*number_machine):
+            # env.render()
+            # input(state)
+            action = agent.act(state)
+            # input(action)
+            action_list.append(action)
+            next_state, score, done = problem.Step(action)
+            reward = oldscore - score + 15 if not done else -1000
+            oldscore = score
+            next_state = np.reshape(next_state, [1,state_size,1])
+            next_state = [next_state[0][i] for i in range(number_job)]
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            # input(reward)
+            # input('-------')
+            if done:
+                print("episode: {}/{}, loop: {}, e: {:.2}"
+                      .format(e, EPISODES, time, agent.epsilon))
+                break
+
+        if len(agent.memory) > batch_size:
+            print('replay')
+            agent.replay(batch_size)
+        
+        
+        # problem.PlotResult() 
+        history.append(score)
+        if e % 10 == 0:
+            meanscore = np.mean( score )
+            print(e, meanscore, agent.epsilon)
+            print(action_list)
+            f = open('his', 'a')
+            f.write(str(meanscore)+'\n')
+            f.close()
+
+        # if e % 10 == 0:
+        #     agent.save("./save/cartpole-dqn.h5")
+
+    # problem = JobShop.JobShop(4, 5, 15, 30)
+    # po = problem.Get_Possible_Job_Position()
+    # problem.MeasurementAction(problem.schedule_line)
+    # problem.Get_Features(po)
+    # print(a)
